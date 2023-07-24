039d6aff1503f355d669dd7201600d633d65d669
senwere <simeonenwere@gmail.com>
2023-07-24 08:55:49 +0100

    parcel tracking app 2

--- login.js
+++ login.js
@@ -1,3 +1,230 @@
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const usee = document.getElementById('usee');
+//   const names = document.getElementById('nxt');
+
+//   // Hide all sections except the first one
+//   const sections = document.querySelectorAll('section');
+//   sections.forEach((section, index) => {
+//     if (index !== 0) {
+//       section.style.display = 'none';
+//     }
+//   });
+
+//   // Add event listener to the link
+//   usee.addEventListener('click', showNextSection);
+
+//   function showNextSection() {
+//     // Check if username or email and password fields are empty
+//     const usernameOrEmail = document.getElementById('usernameOrEmail').value;
+//     const password = document.getElementById('password').value;
+
+//     if (usernameOrEmail.trim() === '' || password.trim() === '') {
+//       alert('Username or email and password fields must not be empty.');
+//       return; // Don't proceed to the next section
+//     }
+
+//     // Find the currently visible section
+//     const visibleSection = document.querySelector('section:not([style*="none"])');
+
+//     // Hide the current section
+//     visibleSection.style.display = 'none';
+
+//     // Show the next section
+//     const nextSection = visibleSection.nextElementSibling;
+//     if (nextSection) {
+//       nextSection.style.display = 'block';
+//     } else {
+//       // If there is no next section, show the first section again
+//       sections[0].style.display = 'block';
+//     }
+//   }
+
+//   // Adjust the section sizes on window resize
+//   window.addEventListener('resize', adjustSectionSizes);
+
+//   function adjustSectionSizes() {
+//     const sections = document.querySelectorAll('section');
+//     const footer = document.querySelector('footer');
+
+//     const footerOffset = footer.offsetTop;
+//     const windowHeight = window.innerHeight;
+
+//     sections.forEach(section => {
+//       const contentHeight = section.scrollHeight;
+//       const sectionMaxHeight = windowHeight - footerOffset;
+
+//       section.style.maxHeight = ''; // Reset max-height property
+
+//       if (contentHeight > sectionMaxHeight) {
+//         section.style.maxHeight = sectionMaxHeight + 'px';
+//       }
+//     });
+//   }
+
+//   // Call the function initially to set the initial section sizes
+//   adjustSectionSizes();
+
+//   let showPasswordToggle = document.getElementById('showPasswordToggle');
+//   let passwordInput = document.getElementById('password');
+
+//   showPasswordToggle.addEventListener('change', function () {
+//     if (this.checked) {
+//       passwordInput.type = 'text';
+//     } else {
+//       passwordInput.type = 'password';
+//     }
+//   });
+
+//   // Function to handle form submission
+//   document.getElementById('loginForm').addEventListener('submit', function (event) {
+//     event.preventDefault(); // Prevent form from submitting
+
+//     // Get form values
+//     let usernameOrEmail = document.getElementById('usernameOrEmail').value;
+//     let password = document.getElementById('password').value;
+
+//     // Check if either of the fields is empty
+//     if (usernameOrEmail.trim() === '' || password.trim() === '') {
+//       alert('Username or email and password fields must not be empty.');
+//       return; // Don't proceed with login
+//     }
+
+//     // Retrieve users from local storage or initialize an empty array
+//     let users = JSON.parse(localStorage.getItem('users')) || [];
+
+//     // Find user by username or email
+//     let foundUser = users.find(function (user) {
+//       return user.username === usernameOrEmail || user.email === usernameOrEmail;
+//     });
+
+//     // Check if user exists and credentials match
+//     if (foundUser && foundUser.password === password) {
+//       // Redirect to main website page
+//       window.location.hash = '#main';
+//       foundUser.loginAttempts = 0;
+
+//       // Save the logged-in user in local storage
+//       localStorage.setItem('currentUser', JSON.stringify(foundUser));
+
+//       // Call the track function
+//       track();
+
+//       // Print the full name of the logged-in user
+//       names.innerHTML = 'Welcome' + ' ' + foundUser.fullName;
+//     } else if (foundUser) {
+//       // User exists but wrong password
+//       if (foundUser.loginAttempts === undefined) {
+//         foundUser.loginAttempts = 1;
+//       } else {
+//         foundUser.loginAttempts++;
+//       }
+//       if (foundUser.loginAttempts >= 3) {
+//         // Redirect to index page after three failed attempts
+//         alert('You have exceeded the maximum number of login attempts. Please try again later.');
+//         window.location.href = 'index.html';
+//       } else {
+//         alert('Invalid password. Please try again.');
+//       }
+//     } else {
+//       // User does not exist, redirect to signup page
+//       alert('No such user found. Please go to sign up.');
+//       window.location.href = 'index.html';
+//     }
+
+//     // Save updated users array to local storage
+//     localStorage.setItem('users', JSON.stringify(users));
+//   });
+
+//   // Function to handle tracking
+//   function track(event) {
+//     event.preventDefault();
+//     console.log('tracking')
+//     if (!checkLogin()) {
+//       alert("Please Login First");
+//       return;
+//     }
+
+//     let trackingId = document.querySelector("#trackingId").value;
+//     let productDetail = document.querySelector(".product-detail");
+
+//     let product = {
+//       123456: {
+//         id: "123432456",
+//         name: "iPhone 12",
+//         status: "Delivered",
+//       },
+//       456789: {
+//         id: "43543",
+//         name: "iPhone 11",
+//         status: "Out for delivery",
+//       },
+//       987654: {
+//         id: "987642",
+//         name: "iPhone 10",
+//         status: "Shipped",
+//       },
+//       654321: {
+//         id: "3245234543",
+//         name: "iPhone 9",
+//         status: "Cancelled",
+//       },
+//     };
+
+//     if (!product.hasOwnProperty(trackingId)) {
+//       productDetail.innerHTML = "<h4>No Product Found!<br>Check Your Details</h4>";
+//       return;
+//     }
+
+//     let selectedProduct = product[trackingId];
+//     console.log(selectedProduct.id)
+//     let productId = document.getElementById("productId");
+//     let productName = document.getElementById("productName");
+//     let productStatus = document.getElementById("productStatus");
+
+//     productId.innerHTML = selectedProduct.id;
+//     productName.innerHTML = selectedProduct.name;
+//     productStatus.innerHTML = selectedProduct.status;
+
+//     let trackingDetail = document.querySelector(".detail");
+//     trackingDetail.style.visibility = 'visible';
+//   }
+
+//   // Function to handle tracking another product
+//   function trackAnother(event) {
+//     event.preventDefault();
+//     document.querySelector(".detail").style.visibility = 'hidden';
+//   }
+
+//   // Function to handle user logout
+//   function logout() {
+//     localStorage.removeItem("currentUser");
+//     alert("Logged Out Successfully");
+//     // Hide the welcome message
+//     names.innerHTML = '';
+//     // Update the login status on the page
+//     let loginButton = document.querySelector(".loginInButton");
+//     loginButton.innerHTML = '<a onclick="showLogin(event)" href="#">Login</a>';
+//   }
+
+//   // Function to check if a user is logged in
+//   function checkLogin() {
+//     let currentUser = localStorage.getItem("currentUser");
+//     if (currentUser && currentUser.length > 0) {
+//       return true;
+//     }
+//     return false;
+//   }
+
+//   // Attach the event listener to the tracking button
+//   let trackButton = document.querySelector(".tracking-button");
+//   trackButton.addEventListener("click", track);
+// });
+
+
+
+
+
 const usee = document.getElementById('usee');
 const names = document.getElementById('nxt');
 
@@ -44,9 +271,20 @@ window.addEventListener('resize', adjustSectionSizes);
 
 function adjustSectionSizes() {
   const sections = document.querySelectorAll('section');
+  const footer = document.querySelector('section');
+
+  const footerOffset = footer.offsetTop;
+  const windowHeight = window.innerHeight;
 
   sections.forEach(section => {
-    section.style.height = window.innerHeight + 'px';
+    const contentHeight = section.scrollHeight;
+    const sectionMaxHeight = windowHeight - footerOffset;
+
+    section.style.maxHeight = ''; // Reset max-height property
+
+    if (contentHeight > sectionMaxHeight) {
+      section.style.maxHeight = sectionMaxHeight + 'px';
+    }
   });
 }
 
@@ -101,16 +339,17 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
   // Check if user exists and credentials match
   if (foundUser && foundUser.password === password) {
+
+  
+
+    // Save the logged-in user in local storage
+    localStorage.setItem('currentUser', JSON.stringify(foundUser));
     // Redirect to main website page
     window.location.hash = '#main';
     foundUser.loginAttempts = 0;
 
-    // Save the logged-in user in local storage
-    localStorage.setItem('currentUser', JSON.stringify(foundUser));
-
     // Call the track function
-   
-   function track(){}
+    track();
 
     // Print the full name of the logged-in user
 
@@ -225,3 +464,6 @@ function checkLogin() {
   }
   return false;
 }
+ // Attach the event listener to the tracking button
+ let trackButton = document.querySelector(".tracking-button");
+ trackButton.addEventListener("click", track);
--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// for index page
+const menuIcon = document.querySelector(".menu-icon");
+const menuItems = document.querySelector(".menu-items");
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("clicked"); // Toggle the 'clicked' class on menu icon
+  menuItems.style.display = menuItems.style.display === "none" ? "flex" : "none";
+});
+
+
 // Create account form submission
 document.getElementById('signupForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent form from submitting
 
   // Get form values
   var fullName = document.getElementById('fullName').value;
-  var address = document.getElementById('address').value;
-  var dob = document.getElementById('dob').value;
   var gender = document.querySelector('input[name="gender"]:checked').value;
   var email = document.getElementById('email').value;
-  var phone = document.getElementById('phone').value;
+ 
 
   // Create user object
   var user = {
     fullName: fullName,
-    address: address,
-    dob: dob,
     gender: gender,
     email: email,
-    phone: phone,
     username: "", // Placeholder for username
     password: "" // Placeholder for password
   };
@@ -35,26 +39,31 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
 });
 
 
-var countryCodeSelect = document.getElementById('countryCode');
-var phoneNumberInput = document.getElementById('phone');
+// var countryCodeSelect = document.getElementById('countryCode');
+// var phoneNumberInput = document.getElementById('phone');
 
-// Define an array of countries with their mobile dial codes
-var countries = [
-  { name: 'United States', code: '+1' },
-  { name: 'United Kingdom', code: '+44' },
-  // Add more countries as needed
-];
+// // Define an array of countries with their mobile dial codes
+// var countries = [
+//   { name: 'United States', code: '+1' },
+//   { name: 'United Kingdom', code: '+44' },
+//   // Add more countries as needed
+// ];
 
 // Populate the country code select options
-countries.forEach(function(country) {
-  var option = document.createElement('option');
-  option.text = country.name + ' (' + country.code + ')';
-  option.value = country.code;
-  countryCodeSelect.appendChild(option);
-});
+// countries.forEach(function(country) {
+//   var option = document.createElement('option');
+//   option.text = country.name + ' (' + country.code + ')';
+//   option.value = country.code;
+//   countryCodeSelect.appendChild(option);
+// });
 
 // Set the initial value of the phone number input based on the selected country code
-countryCodeSelect.addEventListener('change', function() {
-  var selectedCountryCode = this.value;
-  phoneNumberInput.value = selectedCountryCode;
-});
+// countryCodeSelect.addEventListener('change', function() {
+//   var selectedCountryCode = this.value;
+//   phoneNumberInput.value = selectedCountryCode;
+// });
+
+// for another
+
+// JavaScript to toggle the visibility of the menu on smaller screens
+
